965918813ba4c0a23d0601670985daceaab83109
finitefields <am34896@gmail.com.tw>
2015-05-12 17:25:43 +0800

    Multiple of two successful but sin---> Square root of 1-cos^2 failure

--- Midterm.cpp
+++ Midterm.cpp
@@ -6,6 +6,7 @@
 int Generate_N(int p, int q, int r);
 int Initial(double *x, double *y, int N);
 int Print_Complex_Vector(double *x, double *y, int N);
+int FFT_callfunction(double *x_r, double *x_i, double *y_r, double *y_i, int N);
 
 int main(){
 	int k, n, N, p, q, r;
@@ -21,7 +22,7 @@ int main(){
  	y_i = (double *) malloc(N*sizeof(double));
 	Initial(x_r, x_i, N);	
 	t1 = clock();
-	
+	FFT_callfunction(x_r, x_i, y_r, y_i, N);
 	t2 = clock();
 	printf("%f secs\n", 1.0*(t2-t1)/CLOCKS_PER_SEC);
 	Print_Complex_Vector(y_r, y_i, N);
@@ -47,10 +48,60 @@ int Initial(double *x, double *y, int N)
 		y[n] = 0; 
 	}
 }
+
 int Print_Complex_Vector(double *x, double *y, int N)
 {
 	for(int n=0;n<N;++n){
 		if(y[n]>=0) printf("%d : %f +%f i\n", n, x[n], y[n]);
 		else printf("%d : %f %f i\n", n, x[n], y[n]);
 	}
+}
+
+int FFT_callfunction(double *x_r, double *x_i, double *y_r, double *y_i, int N){
+	if(N==1){
+		y_r[0] = x_r[0];
+		y_i[0] = x_i[0];
+		return 0; 
+	}
+	
+	double *u_r, *u_i, *v_r, *v_i, w_r, w_i;
+		
+	u_r = (double *) malloc(N*sizeof(double));
+	u_i = (double *) malloc(N*sizeof(double));
+	v_r = (double *) malloc(N*sizeof(double));
+	v_i = (double *) malloc(N*sizeof(double));
+	
+	for(int n=0;n<N/2;n++){
+		u_r[n] = x_r[2*n];
+		u_i[n] = x_i[2*n];
+		u_r[n+N/2] = x_r[2*n+1];
+		u_i[n+N/2] = x_i[2*n+1];
+	}
+	
+	FFT_callfunction(u_r, u_i, v_r, v_i, N/2);
+	FFT_callfunction(u_r+N/2, u_i+N/2, v_r+N/2, v_i+N/2, N/2);
+	
+	double t_r,t_i;
+	t_r = cos(-2*M_PI/N);
+	t_i = sin(-2*M_PI/N);
+
+	w_r = t_r;
+	w_i = t_i;
+		
+	for(int k=0;k<N/2;++k){
+		y_r[k] = v_r[k] + (w_r*v_r[k+N/2] - w_i*v_i[k+N/2]);
+		y_i[k] = v_i[k] + (w_r*v_i[k+N/2] + w_i*v_r[k+N/2]);
+		y_r[k+N/2] = v_r[k] - (w_r*v_r[k+N/2] - w_i*v_i[k+N/2]);
+		y_i[k+N/2] = v_i[k] - (w_r*v_i[k+N/2] + w_i*v_r[k+N/2]);
+		w_r = w_r*t_r - w_i*t_i;
+		w_i = w_r*t_i + w_i*t_r; 	
+	}
+	
+	free(u_r);
+	free(u_i);
+	free(v_r);
+	free(v_i);
+	
+	return 0;
+	
 }
